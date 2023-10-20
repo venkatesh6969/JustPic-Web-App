93ac88796eea95ae36dfd72a8a0100130648476e
Hrithikshetty <hrithik.cd21@sahyadri.edu.in>
2023-10-20 13:59:45 +0530

    JustPic

--- user-side/src/components/layout/Footer.jsx
+++ user-side/src/components/layout/Footer.jsx
@@ -1,9 +1,15 @@
-import './footer.css';
+import React from "react";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
-      <h1>footer</h1>
+    <div className="footer">
+      <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
     </div>
   );
 };
+
 export default Footer;
